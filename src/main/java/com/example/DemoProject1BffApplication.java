9f1415ca58b7bc783bd96587a92715c86f8958fa
root <root@demo-project-1-acs-bootstrap-xq4rh>
2024-11-05 12:16:48 +0000

    Initial commit

---
+++ src/main/java/com/example/DemoProject1BffApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject1BffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject1BffApplication.class, args);
+	}
+
+}
